362fd729c931ad1940410d012061f8272de5c052
Abdul Bashir Rezaie <abdulbashirrezaie@gmail.com>
2022-05-05 21:39:43 +0430

    Create binary_search_704

---
+++ binary_search_704.py
@@ -0,0 +1,15 @@
+from typing import List
+
+def search(nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            pivot = left + (right - left) // 2
+            if nums[pivot] == target:
+                return pivot
+            if target < nums[pivot]:
+                right = pivot - 1
+            else:
+                left = pivot + 1
+        return -1
+
+print(search([0,1,2,3,12,20], 3))
